73d2bc839904df0b29bdc25da374ca3482f19c78
aaronburt <email@aaronburt.co.uk>
2024-11-05 07:33:42 +0000

    feat: :label: Added LICENSE, fmt'd README - responder.ts, amended deno.json, deno-test.yml
    Formatted Deno.json to add fmt rules, publish rules, and package name. Formatted using packages. added fmt rules to deno-test.yml

--- main_test.ts
+++ main_test.ts
@@ -3,61 +3,60 @@ import DynamicResponder from "./main.ts";
 
 const testPort = 8080;
 
-/* SERVER */ 
+/* SERVER */
 const app = new DynamicResponder();
 
 app.get("/user/:id", (req: Request) => {
-    return new Response(req.params.id);
+	return new Response(req.params.id);
 });
 
 app.get("/:id", (req: Request) => {
-    return new Response(req.params.id);
+	return new Response(req.params.id);
 });
-  
+
 app.get("/", (_req: Request) => {
-    return new Response("This is a standard response");
+	return new Response("This is a standard response");
 });
 
 app.start(testPort);
 
 Deno.test(async function userParamResponse() {
-    const response = await fetch(`http://localhost:${testPort}/user/testaccount`);
-    const text = await response.text();
-    assertEquals(text, "testaccount", "User param response is unexpected");
+	const response = await fetch(`http://localhost:${testPort}/user/testaccount`);
+	const text = await response.text();
+	assertEquals(text, "testaccount", "User param response is unexpected");
 });
 
 Deno.test(async function idParamResponse() {
-    const response = await fetch(`http://localhost:${testPort}/test`);
-    const text = await response.text();
-    assertEquals(text, "test", "ID param response is unexpected");
+	const response = await fetch(`http://localhost:${testPort}/test`);
+	const text = await response.text();
+	assertEquals(text, "test", "ID param response is unexpected");
 });
 
 Deno.test(async function defaultResponse() {
-    const response = await fetch(`http://localhost:${testPort}/`);
-    const text = await response.text();
-    assertEquals(text, "This is a standard response", "Default response is unexpected");
+	const response = await fetch(`http://localhost:${testPort}/`);
+	const text = await response.text();
+	assertEquals(text, "This is a standard response", "Default response is unexpected");
 });
 
 Deno.test(async function invalidUserParam() {
-    const response = await fetch(`http://localhost:${testPort}/user/invalid`);
-    const text = await response.text();
-    assertEquals(text, "invalid", "Invalid user param response is unexpected");
+	const response = await fetch(`http://localhost:${testPort}/user/invalid`);
+	const text = await response.text();
+	assertEquals(text, "invalid", "Invalid user param response is unexpected");
 });
 
 Deno.test(async function nonexistentRoute() {
-    const response = await fetch(`http://localhost:${testPort}/thisisnt/nonexistent`);
-    await response.body?.cancel() // Cancelled the body to remove memory leaks
-    assertEquals(response.status, 404, "Nonexistent route did not return 404");
+	const response = await fetch(`http://localhost:${testPort}/thisisnt/nonexistent`);
+	await response.body?.cancel(); // Cancelled the body to remove memory leaks
+	assertEquals(response.status, 404, "Nonexistent route did not return 404");
 });
 
 Deno.test(async function rootWithQueryParams() {
-    const queryParam = "foo=bar";
+	const queryParam = "foo=bar";
 
-    const response = await fetch(`http://localhost:${testPort}/?${queryParam}`);
-    const responseUrl = new URL(response.url);
-    const responseSearchParam: string = responseUrl.searchParams.toString()
-    const text = await response.text();
-    assertEquals(responseSearchParam, queryParam, "Query param is response is unexpected");
-    assertEquals(text, "This is a standard response", "Root with query params response is unexpected");
+	const response = await fetch(`http://localhost:${testPort}/?${queryParam}`);
+	const responseUrl = new URL(response.url);
+	const responseSearchParam: string = responseUrl.searchParams.toString();
+	const text = await response.text();
+	assertEquals(responseSearchParam, queryParam, "Query param is response is unexpected");
+	assertEquals(text, "This is a standard response", "Root with query params response is unexpected");
 });
-
